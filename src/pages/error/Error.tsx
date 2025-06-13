3a637def56174c3460c338e0465974f59190bafb
Maks <maks.chojniak@gmail.com>
2025-06-13 10:15:32 +0200

    fix error page issue. (remove content of page)

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,5 +1,5 @@
 // import { useRouteError, Link } from 'react-router';
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
 import './style/Error.css';
 
 const Error = () => {
