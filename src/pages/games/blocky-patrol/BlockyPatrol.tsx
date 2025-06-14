8e607b94de05c1aa4c393bfaf5fcacd416d349cd
Maks <maks.chojniak@gmail.com>
2025-06-14 13:13:36 +0200

    add link also to games

--- src/pages/games/blocky-patrol/BlockyPatrol.tsx
+++ src/pages/games/blocky-patrol/BlockyPatrol.tsx
@@ -1,15 +1,16 @@
 import PageSectionName from './../../../components/content/page-section/Section'
 import PageTitle from './../../../components/content/title/Title'
+import PageLinkToContent from './../../../components/content/link-to-content-page/Link'
 import PageContent from './../../../components/content/content/Content'
 
 import './style/BlockyPatrol.css'
 
 const BlockyPatrol = () => {
-
   return (
     <>
     <PageSectionName text={["Games"]}/>
     <PageTitle text={"Blocky Patrol"} />
+    <PageLinkToContent link={"https://play.google.com/store/apps/details?id=com.MMK.BlockyPatrol"}>https://play.google.com/store/BlockyPatrol</PageLinkToContent>
     <PageContent>
       Content
     </PageContent>
--- src/pages/games/iso-racer/IsoRacer.tsx
+++ src/pages/games/iso-racer/IsoRacer.tsx
@@ -1,15 +1,16 @@
 import PageSectionName from './../../../components/content/page-section/Section'
 import PageTitle from './../../../components/content/title/Title'
+import PageLinkToContent from './../../../components/content/link-to-content-page/Link'
 import PageContent from './../../../components/content/content/Content'
 
 import './style/IsoRacer.css'
 
 const IsoRacer = () => {
-
   return (
     <>
     <PageSectionName text={["Games"]}/>
     <PageTitle text={"Iso Racer"} />
+    <PageLinkToContent link={"https://store.steampowered.com/app/2234020/Iso_Racer/"}>https://store.steampowered.com/Iso_Racer/</PageLinkToContent>
     <PageContent>
       Content
     </PageContent>
