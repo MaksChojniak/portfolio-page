fcbd00e0dc80b14a8d2927319fe4e9b42e8afb00
Maks <maks.chojniak@gmail.com>
2025-09-22 17:20:58 +0200

    remove useless for now features

--- src/components/pdf/PDF.tsx
+++ src/components/pdf/PDF.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Document, Page  } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -11,38 +11,38 @@ interface PdfProps {
 
 const PDF = ( {path} : PdfProps ) => {
 
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  // const [numPages, setNumPages] = useState<number>();
+  // const [pageNumber, setPageNumber] = useState<number>(1);
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
-  function changePage(offset: number) {
-    setPageNumber(pageNumber + offset);
-  }
+  // function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+  //   setNumPages(numPages);
+  // }
+  // function changePage(offset: number) {
+  //   setPageNumber(pageNumber + offset);
+  // }
 
 
-  const previous_page_button = (
-  <>
-  <button className='pdf-change-page' onClick={() => changePage(-1)}>
-    Previous
-  </button>
-  </>
-  )
-  const next_page_button = (
-  <>
-  <button className='pdf-change-page' onClick={() => changePage(1)}>
-    Next
-  </button>
-  </>
-  )
+  // const previous_page_button = (
+  // <>
+  // <button className='pdf-change-page' onClick={() => changePage(-1)}>
+  //   Previous
+  // </button>
+  // </>
+  // )
+  // const next_page_button = (
+  // <>
+  // <button className='pdf-change-page' onClick={() => changePage(1)}>
+  //   Next
+  // </button>
+  // </>
+  // )
 
   return (
     <>
-    <Document className="pdf-preview-container" file={path} scale={1.35} onLoadSuccess={onDocumentLoadSuccess}>
-    {/* <Document className="pdf-preview-container" file={path} scale={1.35}> */}
-      <Page className="pdf-preview" pageNumber={pageNumber} renderAnnotationLayer={false} renderTextLayer={false}/>
-      {/* <Page className="pdf-preview" pageNumber={2} renderAnnotationLayer={false} renderTextLayer={false}/> */}
+    {/* <Document className="pdf-preview-container" file={path} scale={1.35} onLoadSuccess={onDocumentLoadSuccess}> */}
+    <Document className="pdf-preview-container" file={path} scale={1.35}>
+      {/* <Page className="pdf-preview" pageNumber={pageNumber} renderAnnotationLayer={false} renderTextLayer={false}/> */}
+      <Page className="pdf-preview" pageNumber={2} renderAnnotationLayer={false} renderTextLayer={false}/>
     </Document>
     {/* <div className='pdf-toolbar'>
       {pageNumber <= 1 ? <></> : previous_page_button}
