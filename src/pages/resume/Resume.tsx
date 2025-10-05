fece72969930b4cf6502a0b92171c497b6790ccb
Maks <maks.chojniak@gmail.com>
2025-10-05 13:36:18 +0200

    add cerifications and exerience

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -20,7 +20,6 @@ const Resume = () => {
 
   const [language, setLanguage] = useState<Language>("en");
   const path = language === "pl" ? resume_path_pl : resume_path_en;
-  // const path = '/assets/resume/test-many-pages.pdf';
 
   const pdf_file_preview = (
   <>
@@ -43,6 +42,16 @@ const Resume = () => {
   </>
   )
 
+  const experience = [
+    <div className='experience-container'><a className='experience' href="https://store.steampowered.com/app/2234020/Iso_Racer/" >Iso Racer</a>2020 - 2023</div>, 
+    <div className='experience-container'><a className='experience' href="https://play.google.com/store/apps/details?id=com.MMK.BlockyPatrol" >Blocky Patrol</a>2023 - 2025</div>
+  ];
+
+  const certifcations = [
+    <a className='certification' href="" >C#</a>, 
+    <a className='certification' href="" >Introduction to Generative AI</a>
+  ];
+
   return (
     <>
     <PageSectionName text={["Portfolio"]} />
@@ -53,11 +62,11 @@ const Resume = () => {
           <SectionRow title='Name' content='Maksymilian Chojniak' />
           <SectionRow title='Role' content='Programmer / Game Developer' />
           <SectionRow title='Description' content='I am a second-year Computer Science student at the Częstochowa University of Technology, actively developing my skills in game and software development. I combine analytical abilities with creativity, and my sports background – including the title of Polish Basketball Champion and numerous individual awards – has taught me teamwork, determination, and consistency in action. I am a fast learner, adapt well to new environments, and approach challenges with enthusiasm.' />
-          <SectionRow title='Experience' content={['Iso Racer 2020 - 2023', 'Blocky Patrol 2023 - 2025']} />
+          <SectionRow title='Experience' contentElements={experience} />
           <SectionRow title='Education' content={['XCIX Liceum Ogólnokształcące z Oddziałami Dwujęzycznymi im.Zbigniewa Herberta, 2020 - 2024', 'Częstochowa University of Technology, Computer Science, 2024 - present']} />
           <SectionRow title='Skills' contentElements={<Skills skills={[ {name: "C#", level: 4}, {name: "Unity", level: 3}, {name: "Algorithms", level: 3}, {name: "C++", level: 3}, {name: "Python", level: 3}, {name: ".NET", level: 3}, {name: "Git", level: 3} ]} max_level={5} />} />
           <SectionRow title='Interests' content='Basketball, Cycling, Traveling' />
-          <SectionRow title='Certificates' content={['C#', 'Introduction to Generative AI']} />
+          <SectionRow title='Certificates' contentElements={certifcations} />
           <SectionRow title='Languages' content='Polish (native), English (B2), French (A1)' />
         </Table>
       </div>
