7acef9e17ebed736ae33fbef29c39979a4f058d2
Maks <maks.chojniak@gmail.com>
2025-10-15 22:34:11 +0200

    change home page styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,32 +3,95 @@ import PageContent from './../../components/content/content/Content'
 
 import './style/Home.css'
 
+
+export const H1 = ({ value }: { value: string } ) => {
+  return (
+    <div className='home-h1' >{value}</div>
+  )
+}
+
+export const H2 = ({ value }: { value: string } ) => {
+  return (
+    <div className='home-h2' >{value}</div>
+  )
+}
+
+// export const H3 = ({ value }: { value: string } ) => {
+//   return (
+//     <div className='home-h3' >{value}</div>
+//   )
+// }
+
+export const ContentLink = ({ link, value, className }: { link: string, value: string, className? : string } ) => {
+  return (
+    <a className={"home-list-element-link "+className} href={link}>{value}</a>
+  )
+}
+
+export const ListItem = ({ children }: { children: React.ReactNode} ) => {
+  return (
+    <dd className="home-list-element">{children}</dd>
+  )
+}
+
+export const Section = ({ children }: { children: React.ReactNode} ) => {
+  return (
+    <div className="home-section">{children}</div>
+  )
+}
+ 
+
 const Home = () => {
 
   return (
     <>
     <PageTitle text={"Home"} />
     <PageContent>
-Hi, I’m Maks Chojniak.
-Developer & indie game creator.
+      <Section>
+        <H1 value={"Hi, I’m Maks Chojniak."}/>
+        <div>Developer & indie game creator.</div>
+      </Section>
 
-I love combining creativity with programming — from early prototypes to fully playable projects with custom mechanics and visuals.
+      <Section>
+        <div>I love combining creativity with programming — from early prototypes to fully playable projects with custom mechanics and visuals.</div>
 
-On this site, you’ll find my favorite works, technical challenges I’ve tackled, and ways to support future development.
+        <div>On this site, you’ll find my favorite works, technical challenges I’ve tackled, and ways to support future development.</div>
+      </Section>
 
-Featured Games:
-  -Blocky Patrol — a fast-paced puzzle platformer
-  -ISO Racer — an isometric racing game
+      <Section>
+        <dl>
+          <H2 value={"Featured Games:"}/>
+          <ListItem>
+            <ContentLink link={""} value={"Blocky Patrol"}/>
+          </ListItem>
+          <ListItem>
+            <ContentLink link={""} value={"ISO Racer"}/>
+          </ListItem>
+        </dl>
+      </Section>
 
-Other Projects:
-    Unity:
-      -Inventory System
-    .NET C#:
-      -Console Application
-    C++:
-      -Console Application
-
-Interested in games, code, or collaboration? [Get in touch →Contact Page]
+      <Section>
+        <dl>
+          <H2 value={"Other Projects:"}/>
+          <dt className='home-list-title-element'>Unity:</dt>
+          <ListItem>
+            <ContentLink link={""} value={"Inventory System"}/>
+          </ListItem>
+          <dt className='home-list-title-element'>.NET C#:</dt>
+          <ListItem>
+            <ContentLink link={""} value={"Console Application"}/>
+          </ListItem>
+          <dt className='home-list-title-element'>C++:</dt>
+          <ListItem>
+            <ContentLink link={""} value={"Console Application"}/>
+          </ListItem>
+        </dl>
+      </Section>
+      
+      <Section>
+        <div>Interested in games, code, or collaboration? <ContentLink className={"home-list-element"} link={""} value={"[Get in touch]"}/></div>
+      </Section>
+    
     </PageContent>
     </>
   )
