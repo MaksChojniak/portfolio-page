01e6a13c2bdb81b9802bfc6110b71fac57fcef7e
Maks <maks.chojniak@gmail.com>
2025-12-28 19:54:20 +0100

    add serializable collections project page

--- src/components/content/code/Code.tsx
+++ src/components/content/code/Code.tsx
@@ -1,19 +1,56 @@
+import { useState, useEffect} from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 import './style/Code.css'
 
 
+const ProgrammingLang =
+{ 
+  csharp: 'csharp', 
+  python: 'python', 
+  cpp: 'cpp', 
+  css: 'css', 
+  html: 'html', 
+  js: 'javascript'
+}
+type ProgrammingLang = (typeof ProgrammingLang)[keyof typeof ProgrammingLang];
+export { ProgrammingLang };
+
+
 interface CodeProps{
-    code: string;
+    code?: string;
+    children: React.ReactNode
+    language: ProgrammingLang
 }
 
-const Code = ( { code } : CodeProps) => {
 
+function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState(
+    window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const listener = () => setMatches(media.matches);
+
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
+  }, [query]);
+
+  return matches;
+}
+
+const Code = ( { code, children, language } : CodeProps) => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  if(code)
+    children = (<>{code}</>)
+  
   return (
     <>
-    <SyntaxHighlighter language="csharp" style={vscDarkPlus} showLineNumbers codeTagProps={{style: { fontSize: "1.1rem" }}}>
-        {code}
+    <SyntaxHighlighter language={language} style={vscDarkPlus} showLineNumbers codeTagProps={{style: { fontSize: !isMobile ? "1rem" : "0.85rem" }}}>
+        {children}
     </SyntaxHighlighter>
     </>
   )
--- src/pages/projects/unity/serializable-collections/SerializableCollections.tsx
+++ src/pages/projects/unity/serializable-collections/SerializableCollections.tsx
@@ -3,7 +3,9 @@ import PageTitle from '../../../../components/content/title/Title'
 import PageLinkToContent from '../../../../components/content/link-to-content-page/Link'
 import PageContent from '../../../../components/content/content/Content'
 
-// import Image from '../../../../components/video/Image'
+import Code, { ProgrammingLang } from '../../../../components/content/code/Code'
+
+import { IoMdDownload } from "react-icons/io";
 
 import './../../style/style.css'
 
@@ -18,11 +20,114 @@ const SerializableCollections = () => {
         <PageLinkToContent link={"https://github.com/MaksChojniak/CDPR-RPG/tree/feature/collections"}>https://github.com/unity/collections</PageLinkToContent>
         <PageContent>
             <div className='project-container'>
-                
                 <div className='project-section'>
-                    <div className='games-section-title'>Collections</div>
+                    <div className='project-section-title'><div className='project-title-icon'><IoMdDownload /></div>Installation</div>
                     <div>
+                        Not released yet
+                    </div>
+                </div>                
+              
+                <div className='project-section'>
+                    <div className='project-section-title'>{'Dictionary<Key, Value>'}</div>
+                    <div>
+                        This custom <b>Dictonary</b> can be created using one of the <b>constructors</b> like in the system collection <b>Dictionary</b>.<br/>
+                        <ul>
+                            <li className='project-list-element'>
+                                Base constructor
+                                <Code language={ProgrammingLang.csharp}>{'UnityDictionary<string, string> dict = new UnityDictionary<string, string>();'}</Code>
+                            </li>
+                            <li className='project-list-element'>
+                                Copy constructor (also from base system dictionary)
+                                <Code language={ProgrammingLang.csharp}>{'UnityDictionary<string, string> dict = new UnityDictionary<string, string>(existingDict);'}</Code>
+                            </li>
+                        </ul>
+                        This data collection also provides all the same functionality as the base one, such as: <b>Add</b>, <b>Remove</b>, <b>ContainsKey</b> and more.<br/>
+                        Example code
+                        <Code language={ProgrammingLang.csharp}>
+                            {'UnityDictionary<string, string> dict = new();' + '\n' +
+                            'dict.Add("key1", "hello");' + '\n' +
+                            'dict.Add("key2", "world");' + '\n' +
+                            'foreach(var el in dict)' + '\n' +
+                            '   Console.WriteLine(el);  // expected output: [key1, hello] [key2, world]'};
+                        </Code>
+                    </div>
+                </div>
 
+                <div className='project-section'>
+                    <div className='project-section-title'>{'HashSet<Value>'}</div>
+                    <div>
+                        This custom <b>HashSet</b> can be created using one of the <b>constructors</b> like in the system collection <b>HashSet</b>.<br/>
+                        <ul>
+                            <li className='project-list-element'>
+                                Base constructor
+                                <Code language={ProgrammingLang.csharp}>{'UnityHashSet<string> hashSet = new UnityHashSet<string>();'}</Code>
+                            </li>
+                            <li className='project-list-element'>
+                                Copy constructor (also from base system hash set)
+                                <Code language={ProgrammingLang.csharp}>{'UnityHashSet<string> hashSet = new UnityHashSet<string>(existingHashSet);'}</Code>
+                            </li>
+                        </ul>
+                        This data collection also provides all the same functionality as the base one, such as: <b>Add</b>, <b>Remove</b>, <b>Clear</b> and more.<br/>
+                        Example code
+                        <Code language={ProgrammingLang.csharp}>
+                            {'UnityHashSet<string> hashSet = new();' + '\n' +
+                            'hashSet.Add("hello");' + '\n' +
+                            'hashSet.Add("world");' + '\n' +
+                            'hashSet.Add("world");' + '\n' +
+                            'foreach(var el in hashSet)' + '\n' +
+                            '   Console.WriteLine(el);  // expected output: [hello] [world]'};
+                        </Code>
+                    </div>
+                </div>
+
+                <div className='project-section'>
+                    <div className='project-section-title'>{'Queue<Value>'}</div>
+                    <div>
+                        This custom <b>Queue</b> can be created using one of the <b>constructors</b> like in the system collection <b>Queue</b>.<br/>
+                        <ul>
+                            <li className='project-list-element'>
+                                Base constructor
+                                <Code language={ProgrammingLang.csharp}>{'UnityQueue<string> queue = new UnityQueue<string>();'}</Code>
+                            </li>
+                            <li className='project-list-element'>
+                                Copy constructor (also from base system queue)
+                                <Code language={ProgrammingLang.csharp}>{'UnityQueue<string> queue = new UnityQueue<string>(existingQueue);'}</Code>
+                            </li>
+                        </ul>
+                        This data collection also provides all the same functionality as the base one, such as: <b>Enqueue</b>, <b>Dequeue</b>, <b>Peek</b>, <b>Clear</b> and more.<br/>
+                        Example code
+                        <Code language={ProgrammingLang.csharp}>
+                            {'UnityQueue<string> queue = new();' + '\n' +
+                            'queue.Enqueue("hello");' + '\n' +
+                            'queue.Enqueue("world");' + '\n' +
+                            'while(queue.Count > 0)' + '\n' +
+                            '   Console.WriteLine(queue.Dequeue());  // expected output: [hello] [world]'};
+                        </Code>
+                    </div>
+                </div>
+                <div className='project-section'>
+                    <div className='project-section-title'>{'Stack<Value>'}</div>
+                    <div>
+                        This custom <b>Stack</b> can be created using one of the <b>constructors</b> like in the system collection <b>Stack</b>.<br/>
+                        <ul>
+                            <li className='project-list-element'>
+                                Base constructor
+                                <Code language={ProgrammingLang.csharp}>{'UnityStack<string> stack = new UnityStack<string>();'}</Code>
+                            </li>
+                            <li className='project-list-element'>
+                                Copy constructor (also from base system stack)
+                                <Code language={ProgrammingLang.csharp}>{'UnityStack<string> stack = new UnityStack<string>(existingStack);'}</Code>
+                            </li>
+                        </ul>
+                        This data collection also provides all the same functionality as the base one, such as: <b>Push</b>, <b>Pop</b>, <b>Peek</b>, <b>Clear</b> and more.<br/>
+                        Example code
+                        <Code language={ProgrammingLang.csharp}>
+                            {'UnityStack<string> stack = new();' + '\n' +
+                            'stack.Push("hello");' + '\n' +
+                            'stack.Push("world");' + '\n' +
+                            'while(stack.Count > 0)' + '\n' +
+                            '   Console.WriteLine(stack.Pop());  // expected output: [world] [hello]'};
+                        </Code>
                     </div>
                 </div>
             
