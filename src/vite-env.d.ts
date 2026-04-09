c656b9f4f3d6f3f51679b8ae11f877cb5ec7d740
MaksChojniak <maks.chojniak@gmail.com>
2026-04-09 08:28:21 +0000

    fix hot-reload

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
-declare module '*.module.css' {
-  const classes: Record<string, string>
-  export default classes
-}
+/// <reference types="vite/client" />
+// declare module '*.module.css' {
+//   const classes: Record<string, string>
+//   export default classes
+// }
